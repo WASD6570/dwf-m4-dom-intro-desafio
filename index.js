6c9a217f7881e135dd96474cfd21d765e6f1f2b8
Eduardo Sanchez <wasd12.ns@gmail.com>
2021-07-12 13:22:53 -0300

    Added: liEliminator and liAdder

--- index.js
+++ index.js
@@ -25,6 +25,30 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function liEliminator(li) {
+  for (let index = 0; index < li.length; index++) {
+    li[index].remove();
+  }
+  li[0].remove();
+  return li;
+}
+
+function liAdder(liCollection, ulList) {
+  liCollection.forEach((element) => {
+    const newLiEl = document.createElement("li");
+    newLiEl.textContent = element.tema;
+    newLiEl.setAttribute("class", element.class);
+    ulList.appendChild(newLiEl);
+  });
+  return ulList;
+}
+
+function main() {
+  const ulLi = document.querySelector(".lista");
+  const oldLiEls = ulLi.getElementsByTagName("li");
+  liEliminator(oldLiEls);
+  liAdder(cosasQueAprendimos, ulLi);
+  console.log(ulLi);
+}
 
 main();
